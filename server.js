8e5f42b9d2572c2f6168b5be3a94757d6d8d938f
kacsokz <caseysokach@gmail.com>
2019-09-30 14:25:39 -0600

    sample users

--- server.js
+++ server.js
@@ -9,4 +9,17 @@ app.get('/', (req, res) => {
   res.send('Hello Team')
 })
 
+app.get('/api/users', (req, res) => {
+  res.send([
+    {
+      "id": 1,
+      "first_name": "Randy",
+      "last_name": "Calhoon",
+      "email": "randy.calhoon@gmail.com",
+      "avatar": "https://i.imgur.com/LpaY82x.png",
+      "tag_line": "Always ready to rock and/or roll!"
+    }
+  ])
+})
+
 app.listen(PORT, () => console.log("Im listening on " + PORT))
