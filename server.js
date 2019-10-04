5c037ed914a296f326d505942f4c8e6467d481d7
Devon Blake <devon.blake@live.com>
2019-10-04 15:13:18 -0600

    add dependencies for session, and socket.io, split out passport to usePassport

--- server.js
+++ server.js
@@ -3,38 +3,17 @@ const dotEnvFilePath = __dirname + '/.env.' + ENV
 require('dotenv').config({ path: dotEnvFilePath });
 
 const PORT = process.env.PORT || 3000;
-const CLIENTID = process.env.CLIENTID
-const CLIENTSECRET = process.env.CLIENTSECRET
-const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const passport = require('passport');
 const session = require('express-session');
 const socketio = require('socket.io');
-const http = require('http')
-
+const http = require('http');
+const usePassport = require('./src/usePassport');
 const app = require("./src/application")(ENV);
 server = http.createServer(app)
 // const server = require("http").Server(app);
+
 app.use(passport.initialize());
-
-passport.use(new LinkedInStrategy({
-  clientID: CLIENTID,
-  clientSecret: CLIENTSECRET,
-  callbackURL: "http://6dbb7a59.ngrok.io/auth/linkedin/callback", //wont work on localhost
-  scope: ['r_emailaddress', 'r_liteprofile'],
-  // state: true
-}, function(accessToken, refreshToken, profile, done) {
-  console.log(accessToken, "token");
-  // asynchronous verification, for effect...
-  process.nextTick(function (req, res) {
-    console.log(req, "requeeeeeeeeeeeeeeeest")
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
-}));
-
+usePassport();
 app.use(session({ 
   secret: 'process.env.SESSION_SECRET', 
   resave: true, 
@@ -46,21 +25,14 @@ app.use(session({
 const io = socketio(server)
 app.set('io', io)
 
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', (error, user, info) => {
 
-  console.log()
-  res.redirect("http://localhost:5000/");
-});
-
-
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-  successRedirect: 'http://localhost:8000/',
-  failureRedirect: '/login',
-  session: false
-  }),
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin'),
   (req, res) => {
-    res.redirect("http://localhost:5000/");
+    console.log(req.user, "USERRRRRRRR")
+    res.redirect('http://localhost:8000?username' + req.user.name.givenName);
   }
 );
+
+app.get('/auth/linkedin', passport.authenticate('linkedin'));
 
 server.listen(PORT, () => console.log("Im listening on " + PORT))
---
+++ src/usePassport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const CLIENTID = process.env.CLIENTID
+const CLIENTSECRET = process.env.CLIENTSECRET
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+
+module.exports = () => {
+// Allowing passport to serialize and deserialize users into sessions
+passport.serializeUser((user, cb) => cb(null, user))
+passport.deserializeUser((obj, cb) => cb(null, obj))
+passport.use(new LinkedInStrategy({
+  clientID: CLIENTID,
+  clientSecret: CLIENTSECRET,
+  callbackURL: "http://6dbb7a59.ngrok.io/auth/linkedin/callback", //wont work on localhost
+  scope: ['r_emailaddress', 'r_liteprofile'],
+  // state: true
+}, function(accessToken, refreshToken, profile, done) {
+    // To keep the example simple, the user's LinkedIn profile is returned to
+    // represent the logged-in user. In a typical application, you would want
+    // to associate the LinkedIn account with a user record in your database,
+    // and return that user instead.
+    return done(null, profile);
+}));
+}
