87c3b6e5de4641b8bdb83ddc18bd1db4cfa29f96
Devon Blake <devon.blake@live.com>
2019-10-04 12:14:50 -0600

    work on linkedin api

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotEnvFilePath = __dirname + '/.env.' + ENV
 require('dotenv').config({ path: dotEnvFilePath });
 
 const PORT = process.env.PORT || 3000;
+const CLIENTID = process.env.CLIENTID
+const CLIENTSECRET = process.env.CLIENTSECRET
 const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const passport = require('passport');
 
@@ -11,22 +13,22 @@ const express = require('express');
 const app = require("./src/application")(ENV);
 // const server = require("http").Server(app);
 
-// passport.use(new LinkedInStrategy({
-//   clientID: CLIENTID,
-//   clientSecret: CLIENTSECRET,
-//   callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback", //wont work on localhost
-//   scope: ['r_emailaddress', 'r_basicprofile'],
-//   // state: true
-// }, function(accessToken, refreshToken, profile, done) {
-//   // asynchronous verification, for effect...
-//   process.nextTick(function () {
-//     // To keep the example simple, the user's LinkedIn profile is returned to
-//     // represent the logged-in user. In a typical application, you would want
-//     // to associate the LinkedIn account with a user record in your database,
-//     // and return that user instead.
-//     return done(null, profile);
-//   });
-// }));
+passport.use(new LinkedInStrategy({
+  clientID: CLIENTID,
+  clientSecret: CLIENTSECRET,
+  callbackURL: "https://together-lhl-api.herokuapp.com/auth/linkedin/callback", //wont work on localhost
+  scope: ['r_emailaddress', 'r_basicprofile'],
+  // state: true
+}, function(accessToken, refreshToken, profile, done) {
+  // asynchronous verification, for effect...
+  process.nextTick(function () {
+    // To keep the example simple, the user's LinkedIn profile is returned to
+    // represent the logged-in user. In a typical application, you would want
+    // to associate the LinkedIn account with a user record in your database,
+    // and return that user instead.
+    return done(null, profile);
+  });
+}));
 
 app.get('/auth/linkedin',
   passport.authenticate('linkedin'),
